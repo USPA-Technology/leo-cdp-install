e399eaf4fffa9c7018fbbc4fd9b0282ca3c784c0
trieu <tantrieuf31.database@gmail.com>
2022-04-13 19:19:50 +0700

    create leocdp user with no password

--- install-leo-cdp.sh
+++ install-leo-cdp.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 sudo apt install git
-sudo adduser leocdp
+sudo useradd leocdp -s /bin/bash -p '*'
+sudo passwd -d leocdp
 sudo usermod -aG sudo leocdp
 echo 'leocdp ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers >/dev/null
-sudo -iu leocdp
-sudo mkdir /build
-sudo chown leocdp:leocdp /build/
-cd /build
-git clone https://github.com/trieu/leo-cdp-free-edition.git leo-cdp
+sudo mkdir /build/
+sudo git clone https://github.com/trieu/leo-cdp-free-edition.git /build/leo-cdp
+sudo chown -R leocdp:leocdp /build/
