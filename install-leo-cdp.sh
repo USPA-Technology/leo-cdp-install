9d058b1cf689ae123aa1330a60bd8f5978c51cd8
Trieu Nguyen <tantrieuf31.database@gmail.com>
2022-04-14 17:47:44 +0700

    Update install-leo-cdp.sh

--- install-leo-cdp.sh
+++ install-leo-cdp.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-# create default user 
+
+# create the user leocdp 
 sudo useradd leocdp -s /bin/bash -p '*'
 sudo passwd -d leocdp
 sudo usermod -aG sudo leocdp
 echo 'leocdp ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers >/dev/null
+
 # make folder to git pull 
 sudo mkdir /build/
 sudo git clone https://github.com/trieu/leo-cdp-free-edition.git /build/leo-cdp
+
+# update permissions
 sudo chown -R leocdp:leocdp /build/
+sudo chmod +x /build/leo-cdp/*.sh
